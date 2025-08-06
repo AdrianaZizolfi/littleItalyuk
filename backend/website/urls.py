b937406d21f79a233a67e97e9c951f1763896bbb
AdrianaZizolfi <adrianazizolfi0@gmail.com>
2025-08-06 16:57:09 +0100

    urls and settings static

--- backend/website/urls.py
+++ backend/website/urls.py
@@ -15,5 +15,8 @@ urlpatterns = [
     path('', include('core.urls')),
 ]
 
+# Add this - serve static files in all environments
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

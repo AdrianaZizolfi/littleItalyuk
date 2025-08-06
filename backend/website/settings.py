d66a102d2df023db04a77aeb8d64d8e8db390279
AdrianaZizolfi <adrianazizolfi0@gmail.com>
2025-08-06 14:38:48 +0100

    settings

--- backend/website/settings.py
+++ backend/website/settings.py
@@ -163,12 +163,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
+
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'frontend', 'dist', 'assets'),
+# ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'frontend', 'dist', 'assets'),
+    os.path.join(BASE_DIR, 'frontend_dist'),  # e.g. /app/frontend_dist in container
 ]
-
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
